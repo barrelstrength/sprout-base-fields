e79e6db357671252cac72e92c1c6bb890b2048e2
Ben Parizek <ben@barrelstrengthdesign.com>
2019-05-21 01:46:30 -0400

    Adds support for Date data type for Predefined field

---
+++ src/migrations/m190521_000000_add_predefined_data_type_setting.php
@@ -0,0 +1,54 @@
+<?php /** @noinspection ClassConstantCanBeUsedInspection */
+
+namespace barrelstrength\sproutbasefields\migrations;
+
+use CommerceGuys\Addressing\Subdivision\SubdivisionRepository;
+use craft\db\Migration;
+use craft\db\Query;
+use craft\helpers\Json;
+
+/**
+ * m190521_000000_add_predefined_data_type_setting migration.
+ */
+class m190521_000000_add_predefined_data_type_setting extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp(): bool
+    {
+        $type = 'barrelstrength\sproutfields\fields\Predefined';
+
+        $predefinedFields = (new Query())
+            ->select('*')
+            ->from('{{%fields}}')
+            ->where([
+                'type' => $type
+            ])
+            ->all();
+
+        foreach ($predefinedFields as $predefinedField) {
+            $settings = Json::decode($predefinedField['settings'], true);
+
+            if (!isset($settings['contentColumnType'])) {
+                $settings['contentColumnType'] = 'text';
+            }
+
+            // Save the new settings
+            $this->update('{{%fields}}', [
+                'settings' => Json::encode($settings)],
+                ['type' => $type], [], false);
+        }
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown(): bool
+    {
+        echo "m190521_000000_add_predefined_data_type_setting cannot be reverted.\n";
+        return false;
+    }
+}
