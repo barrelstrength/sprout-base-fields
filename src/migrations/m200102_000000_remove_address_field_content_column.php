101b88cab1c376406b25c3a310565f872b01c442
Ben Parizek <ben.parizek@gmail.com>
2020-01-05 11:06:42 -0500

    Update address migration

--- src/migrations/m200102_000000_remove_address_field_content_column.php
+++ src/migrations/m200102_000000_remove_address_field_content_column.php
@@ -2,6 +2,7 @@
 
 namespace barrelstrength\sproutbasefields\migrations;
 
+use barrelstrength\sproutbasefields\SproutBaseFields;
 use craft\db\Migration;
 use craft\db\Query;
 use craft\db\Table;
@@ -26,6 +27,30 @@ class m200102_000000_remove_address_field_content_column extends Migration
 
         // SPROUT FIELDS
 
+        $tempAddressFieldsTable = '{{%sproutfields_addresses_temp}}';
+        $addressFieldsTable = '{{%sproutfields_addresses}}';
+
+        if (!$this->db->tableExists($tempAddressFieldsTable)) {
+            // Create a fresh address field table
+            $this->createTable($tempAddressFieldsTable, [
+                'id' => $this->primaryKey(),
+                'elementId' => $this->integer(),
+                'siteId' => $this->integer(),
+                'fieldId' => $this->integer(),
+                'countryCode' => $this->string(),
+                'administrativeAreaCode' => $this->string(),
+                'locality' => $this->string(),
+                'dependentLocality' => $this->string(),
+                'postalCode' => $this->string(),
+                'sortingCode' => $this->string(),
+                'address1' => $this->string(),
+                'address2' => $this->string(),
+                'dateCreated' => $this->dateTime()->notNull(),
+                'dateUpdated' => $this->dateTime()->notNull(),
+                'uid' => $this->uid(),
+            ]);
+        }
+
         // Get all Name fields from content table (Craft / Sprout Forms)
         $addressFieldTypes = (new Query())
             ->select(['id', 'handle', 'settings', 'type'])
@@ -36,9 +61,39 @@ class m200102_000000_remove_address_field_content_column extends Migration
         // Update every Name Column that matches a blank name JSON string and set it to null
         foreach ($addressFieldTypes as $field) {
             $columnName = 'field_'.$field['handle'];
-            if ($this->db->columnExists(Table::CONTENT, $columnName)) {
-                $this->dropColumn(Table::CONTENT, $columnName);
+            if (!$this->db->columnExists(Table::CONTENT, $columnName)) {
+                continue;
             }
+
+            $elementsWithAddressIds = (new Query())
+                ->select(['id', 'elementId', $columnName])
+                ->from([Table::CONTENT])
+                ->where(['not', [$columnName => null]])
+                ->all();
+
+            // Insert new Address records with Element IDs to match existing Elements
+            foreach ($elementsWithAddressIds as $elementsWithAddressId) {
+                $addressId = $elementsWithAddressId[$columnName];
+
+                $address = (new Query())
+                    ->select(['*'])
+                    ->from([$addressFieldsTable])
+                    ->where(['id' => $addressId])
+                    ->one();
+
+                if (!$address) {
+                    SproutBaseFields::info('Unable to migrate address. Unable to find address with ID: '. $addressId. ' for element '. $elementsWithAddressId['elementId']);
+                    continue;
+                }
+
+                $address['elementId'] = $elementsWithAddressId['elementId'];
+                unset($address['id']);
+                $this->insert($tempAddressFieldsTable, $address, false);
+            }
+
+            $this->dropTable($addressFieldsTable);
+            $this->renameTable($tempAddressFieldsTable, $addressFieldsTable);
+            $this->dropColumn(Table::CONTENT, $columnName);
         }
 
         if (!$this->db->tableExists('{{%sproutforms_forms}}')) {
